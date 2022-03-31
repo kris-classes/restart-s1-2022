635e0514e3b0f67d628dd987cee7e993ef16a8d7
Soul <101837861+SoulSol@users.noreply.github.com>
2022-03-31 14:32:23 +1300

    Add files via upload

--- graphics3.py
+++ graphics3.py
@@ -1,8 +1,23 @@
-import turtle
-skk = turtle.Turtle()
- 
-for i in range(4):
-    skk.forward(50)
-    skk.right(90)
-     
+import turtle  
+# Creating turtle  
+t = turtle.Turtle()  
+s = turtle.Screen()  
+s.bgcolor("black")  
+t.pencolor("red")  
+    
+a = 0  
+b = 0  
+t.speed(0)  
+t.penup()  
+t.goto(0,200)  
+t.pendown()  
+while(True):  
+    t.forward(a)  
+    t.right(b)  
+    a+=3  
+    b+=1  
+    if b == 210:  
+        break  
+    t.hideturtle()  
+    
 turtle.done()
