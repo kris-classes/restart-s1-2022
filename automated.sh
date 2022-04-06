cb5255d285a5a9c1bf8a2e346f7062db4fbf6e0f
MatthewKlemick <73146834+MatthewKlemick@users.noreply.github.com>
2022-04-07 09:26:07 +1200

    Update automated.sh

--- automated.sh
+++ automated.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Usage: curl https://raw.githubusercontent.com/kris-classes/restart/main/automated.sh | bash
 #
+curl https://raw.githubusercontent.com/MatthewKlemick/Centos_Scripts/main/re_Start_ascii.sh | bash
 echo "Bash script to install all necessary packages."
 alias la="ls -al"
 sudo yum install -y tmux python3 fish strace zsh file gcc
