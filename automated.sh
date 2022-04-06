b1448948f7ccbfcc2ad9b37cf0b05c2076d65281
Count Zer0 <101837582+Matthew-W-Reynolds@users.noreply.github.com>
2022-04-06 14:57:05 +1200

    Update automated.sh
    MODIFIED BY Matthew Reynolds: cleaned up the script and added gcc

--- automated.sh
+++ automated.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# run with: curl https://raw.githubusercontent.com/kris-classes/restart/main/automated.sh | bash
-echo "helloooo"
-alias lx="ls -al"
-touch cow
-sudo yum install -y tmux
-sudo yum install -y python3
-sudo yum install -y fish
-sudo yum install -y strace zsh file
+# Usage: curl https://raw.githubusercontent.com/kris-classes/restart/main/automated.sh | bash
+#
+echo "Bash script to install all necessary packages."
+alias la="ls -al"
+sudo yum install -y tmux python3 fish strace zsh file gcc
 sudo pip3 install pylint bpython pytest
 echo "set-option repeat-time 10" >> ~/.tmux.conf
