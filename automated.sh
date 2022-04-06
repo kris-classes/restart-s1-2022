9b4b9f9f09ec6c409a77353530ff854a040d1c6c
Jessedunstan <101837586+Jessedunstan@users.noreply.github.com>
2022-04-06 14:53:07 +1200

    Update automated.sh

--- automated.sh
+++ automated.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# run with: curl https://raw.githubusercontent.com/kris-classes/restart/main/automated.sh | bash
 echo "helloooo"
 alias lx="ls -al"
 touch cow
 sudo yum install -y tmux
+sudo yum install -y python3
+sudo yum install -y fish
+sudo yum install -y strace zsh file
+sudo pip3 install pylint bpython pytest
+echo "set-option repeat-time 10" >> ~/.tmux.conf
