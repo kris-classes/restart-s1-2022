371c85de0d5bc69304bd777483323203087b00d1
Count Zer0 <101837582+Matthew-W-Reynolds@users.noreply.github.com>
2022-04-14 09:06:20 +1200

    Update automated.sh
    removed ftop - Not found on any of the mirrors

--- shell_scripts/automated.sh
+++ shell_scripts/automated.sh
@@ -4,6 +4,6 @@
 curl https://raw.githubusercontent.com/MatthewKlemick/Centos_Scripts/main/re_Start_ascii.sh | bash
 echo "Bash script to install all necessary packages."
 alias la="ls -al"
-sudo yum install -y tmux python3 fish strace zsh file gcc netcat tcpdump ftop
+sudo yum install -y tmux python3 fish strace zsh file gcc netcat tcpdump
 sudo pip3 install pylint bpython pytest
 echo "set-option repeat-time 10" >> ~/.tmux.conf
