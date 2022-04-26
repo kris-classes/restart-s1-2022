bbdcf56f58ca0c79b8b360db6a0f81a53d8ba6f1
Nick Do <linh.do@tyme.com>
2022-04-27 06:10:56 +0700

    React day 2

---
+++ react_day_2/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UseStateExample from "./UseStateExample";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <UseStateExample />
+  </React.StrictMode>
+);
