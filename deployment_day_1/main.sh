faac3e5d73a006684ed4b799779fcfe8fbbd251d
Nick Do <linh.do@tyme.com>
2022-05-11 05:29:28 +0700

    added backend requirements files for react_day_6, and main.sh for automation

---
+++ deployment_day_1/main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Step 0. get the files
+git clone https://github.com/kris-classes/restart.git
+cd restart
+shopt -s extglob # revisit this for Window OS
+rm -r !(react_day_6)
+
+# Step 1. npm install
+cd react_day_6
+npm install
+
+# Step 2. npm build
+npm run build
+
+# Step 3. move folder to where you want
+
+# Step 4. install serve package to deploy React locally
+npm install -g serve
+
+# Step 5. install Python packages with Pip
+cd backend
+pip install -r requirements.txt
+
+# Step 5. start both Uvicorn and Npm Serve
+# frontend & backend && fg
